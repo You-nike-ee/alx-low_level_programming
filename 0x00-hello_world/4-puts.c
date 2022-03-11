874ba472bd5b760b9ed1e3d038e33ca46ed41563
You-nike-ee <lambeeunice16@gmail.com>
2022-03-10 19:27:31 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-1 #include <stdio.h>
-2 /**
-3 * main - main block
-4 * Return : 0
-5 */
-6 int main (void)
-7 {
-8 puts ("\"Programming is like building a multilingual puzzle") ;
-9 return (0) ;
-10 }
-11
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
